667ae6e1e0ac72c4e3134c62c2416ec7c86e14bc
dalcon <dante.alferez@takanadev.com>
2025-08-21 00:27:50 -0500

    OpenAPI was added to has a swagger ui

--- src/main/java/com/takanadev/Application.java
+++ src/main/java/com/takanadev/Application.java
@@ -1,7 +1,16 @@
 package com.takanadev;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Demo API",
+        version = "0.1",
+        description = "Documentation for a Demo API using Micronaut"
+    )
+)
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/takanadev/controller/OrderController.java
+++ src/main/java/com/takanadev/controller/OrderController.java
@@ -4,10 +4,14 @@ import com.takanadev.domain.Order;
 import com.takanadev.repository.OrderRepository;
 import io.micronaut.http.annotation.*;
 import io.micronaut.http.HttpStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
 @Controller("/orders")
+@Tag(name = "Orders")
 public class OrderController {
 
     private final OrderRepository repository;
@@ -17,12 +21,18 @@ public class OrderController {
     }
 
     @Get
+    @Operation(summary = "Get all orders", description = "We can get all orders of the system")
+    @ApiResponse(responseCode = "200", description = "Orders found")
+    @ApiResponse(responseCode = "404", description = "There was an error")
     public List<Order> getAll() {
         return repository.findAll();
     }
 
     @Post
     @Status(HttpStatus.CREATED)
+    @Operation(summary = "Create an order", description = "We can create an order in the system")
+    @ApiResponse(responseCode = "200", description = "Order created")
+    @ApiResponse(responseCode = "404", description = "Order wasn't able to be created")
     public Order create(@Body Order order) {
         return repository.save(order);
     }
